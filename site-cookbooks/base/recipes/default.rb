9683609a75372360d7326071c023cee00367b96e
hideack <hide.terai@gmail.com>
2013-11-03 14:52:06 +0900

    cookbookの配置場所間違い修正

---
+++ site-cookbooks/base/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: base
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+%w{git rbenv bundler curl}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
