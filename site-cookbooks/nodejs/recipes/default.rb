952d2b7282e505db2a3443e4ac0152dfce7c04db
hideack <hide.terai@gmail.com>
2013-12-14 18:14:52 +0900

    node.jsのapt-getするリポジトリを変更

--- site-cookbooks/nodejs/recipes/default.rb
+++ site-cookbooks/nodejs/recipes/default.rb
@@ -6,9 +6,30 @@
 #
 # All rights reserved - Do Not Redistribute
 
-%w{nodejs npm}.each do |pkg|
+%w{software-properties-common python-software-properties python g++ make}.each do |pkg|
   package pkg do
     action :install
   end
 end
 
+execute "add-apt-repository -y ppa:chris-lea/node.js" do
+  user "root"
+end
+
+execute "apt-get update" do
+  user "root"
+end
+
+%w{nodejs}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+bash "grunt install" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+    npm install -g grunt-cli
+  EOH
+end
