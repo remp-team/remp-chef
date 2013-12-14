18a903f5c6ea0e287f3fd404ec54e801c16ffc80
hideack <hide.terai@gmail.com>
2013-12-14 12:13:30 +0900

    nodejs, npmインストール

--- site-cookbooks/nodejs/recipes/default.rb
+++ site-cookbooks/nodejs/recipes/default.rb
@@ -6,7 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 
-%w{python-software-properties python g++ make}.each do |pkg|
+%w{nodejs npm}.each do |pkg|
   package pkg do
     action :install
   end
