d3f1e1d0609d8325613d38609a758aa471d254e2
hideack <hide.terai@gmail.com>
2013-12-15 14:18:13 +0900

    wkhtmltoimage-amd64のインストールレシピ追加

--- site-cookbooks/screenshot/recipes/default.rb
+++ site-cookbooks/screenshot/recipes/default.rb
@@ -2,7 +2,23 @@
 # Cookbook Name:: screenshot
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, REMP Team
 #
 # All rights reserved - Do Not Redistribute
 #
+
+%w{libxrender1 libfontconfig1 otf-ipafont-gothic libxext6}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+bash "wkhtmltoimage install." do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+    wget 'https://wkhtmltopdf.googlecode.com/files/wkhtmltoimage-0.10.0_rc2-static-amd64.tar.bz2';
+    tar xjfv wkhtmltoimage-0.10.0_rc2-static-amd64.tar.bz2
+    mv wkhtmltoimage-amd64 /usr/local/bin
+  EOH
+end
