d630ac5f1a4b123d34e52c0da5fcb3fcc4c81545
hideack <hide.terai@gmail.com>
2014-01-12 14:32:16 +0900

    storyboards001で稼働させるcron設定記述

--- site-cookbooks/storyboards-app-base/recipes/default.rb
+++ site-cookbooks/storyboards-app-base/recipes/default.rb
@@ -2,7 +2,28 @@
 # Cookbook Name:: storyboards-app-base
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Copyright 2014, REMP Team
 #
 # All rights reserved - Do Not Redistribute
 #
+
+cron "Screen shot" do
+  user "remper"
+  command "cd ~/storyboards/current && bundle exec padrino rake screenshot[10] -e production"
+  minute "*/8"
+end
+
+cron "Daily report" do
+  user "remper"
+  command "cd ~/storyboards/current && bundle exec padrino rake daily_report -e production"
+  hour "8"
+  minute "31"
+end
+
+cron "Site map generate" do
+  user "remper"
+  command "cd ~/storyboards/current && bundle exec padrino rake sitemap_generate -e production"
+  hour "14"
+  minute "35"
+end
+
