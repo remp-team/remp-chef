3284e3576c8de8dcc2de50b53578fa5d56c9ac5e
hideack <hide.terai@gmail.com>
2014-01-12 14:17:00 +0900

    storyboards.jpサーバ用base新規作成

---
+++ site-cookbooks/storyboards-app-base/metadata.rb
@@ -0,0 +1,7 @@
+name             'storyboards-app-base'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures storyboards-app-base'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
