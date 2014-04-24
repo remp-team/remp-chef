77d89ea77c09050088d72f7b2b070328c2d0615a
hideack <hide.terai@gmail.com>
2014-04-24 09:53:04 +0900

    supervisorctl用テンプレート配置

--- site-cookbooks/manage/recipes/default.rb
+++ site-cookbooks/manage/recipes/default.rb
@@ -7,17 +7,24 @@
 # All rights reserved - Do Not Redistribute
 #
 
-
-%w{hubot coffee-script}.each do |p|
-  execute p do
-    command "npm install -g #{p}"
-  end
-end
-
-%w{redis-server}.each do |pkg|
+%w{supervisor redis-server}.each do |pkg|
   package pkg do
     action :install
   end
+end
+
+template "/etc/supervisor/supervisord.conf" do
+  source "supervisor/supervisord.conf.erb"
+  mode 0644
+  owner "root"
+  group "root"
+end
+
+template "/etc/supervisor/conf.d/hubot.conf" do
+  source "supervisor/hubot.conf.erb"
+  mode 0644
+  owner "remper"
+  group "remper"
 end
 
 
